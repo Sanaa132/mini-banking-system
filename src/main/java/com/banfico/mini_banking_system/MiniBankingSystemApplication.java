fb98f96e6c91346efbdac5e40f7a86d85445435d
Your Name <you@example.com>
2026-07-15 23:01:10 +0530

    Initial Spring Boot project setup and Configured PostgreSQL datasource

---
+++ src/main/java/com/banfico/mini_banking_system/MiniBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.banfico.mini_banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banfico/mini_banking_system/MiniBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banfico.mini_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
